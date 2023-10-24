1e33fddef6935ac631be6d7609e8ce42e14bd668
Grigoriy <markarian342@gmail.com>
2023-10-24 20:19:55 +0300

    Добавил степень

---
+++ ConsoleApp4/Logic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Logic
+    {
+        public int PowerOf2(int a)
+        {
+            return a * a;
+        }
+    }
+}
